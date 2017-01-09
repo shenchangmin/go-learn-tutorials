8637b25bbd7102aee29075ab619c4d38fa3b50cd
shenchangmin <503228482@qq.com>
2017-01-09 17:34:50 +0800

    传值与传指针

--- src/tutorials/multi/main.go
+++ src/tutorials/multi/main.go
@@ -5,16 +5,26 @@ import "fmt"
 func SumAndProduct(A, B int) (int, int) {
 	return A + B, A * B
 }
+
 func myfunc(arg ...int) {
 	for _, n := range arg {
 		fmt.Printf("and the number is:%d\n", n)
 	}
 }
+
+func add(a *int) int {
+	*a = *a + 1
+	return *a
+}
+
 func main() {
 	myfunc(1, 2, 3, 4, 5)
 	x := 3
 	y := 4
+	x1 := add(&x)
 	xPLUSy, xTIMESy := SumAndProduct(x, y)
+	fmt.Println(x1)
+	fmt.Println(x)
 	fmt.Printf("%d+%d=%d\n", x, y, xPLUSy)
 	fmt.Printf("%d*%d=%d\n", x, y, xTIMESy)
 }
