ff50215deb6ec23e0f0d7a56cd69e5fbce30a138
shenchangmin <503228482@qq.com>
2017-01-10 20:07:43 +0800

    method完成

--- src/tutorials/method/main.go
+++ src/tutorials/method/main.go
@@ -1,37 +1,83 @@
 package main
 
-import (
-	"fmt"
-	"math"
+import "fmt"
+
+const (
+	// WHITE 颜色常量
+	WHITE = iota
+	// BLACK 颜色常量
+	BLACK
+	// BLUE 颜色常量
+	BLUE
+	// RED 颜色常量
+	RED
+	// YELLOW 颜色常量
+	YELLOW
 )
 
-// Rectangle 长方形
-type Rectangle struct {
-	width, height float64
+// Color 颜色
+type Color byte
+
+// Box 盒子
+type Box struct {
+	width, height, depth float64
+	color                Color
 }
 
-// Circle 圆
-type Circle struct {
-	radius float64
+// BoxList a slice of boxes
+type BoxList []Box
+
+// Volume 盒子的体积
+func (b Box) Volume() float64 {
+	return b.width * b.height * b.depth
 }
 
-//计算长方形面积
-func (r Rectangle) area() float64 {
-	return r.width * r.height
+// SetColor 设置盒子的颜色
+func (b Box) SetColor(c Color) {
+	b.color = c
 }
 
-//计算圆的面积
-func (c Circle) area() float64 {
-	return c.radius * c.radius * math.Pi
+// BiggestColor 体积最大的盒子的颜色
+func (bl BoxList) BiggestColor() Color {
+	v := 0.00
+	k := Color(WHITE)
+	for _, b := range bl {
+		if bv := b.Volume(); bv > v {
+			v = bv
+			k = b.color
+		}
+	}
+	return k
 }
 
+// PaintItBlack 将盒子颜色设置成黑色
+func (bl BoxList) PaintItBlack() {
+	for i := range bl {
+		bl[i].SetColor(BLACK)
+	}
+}
+func (c Color) String() string {
+	strings := []string{"WHITE", "BLACK", "BLUE", "RED", "YELLOW"}
+	return strings[c]
+}
 func main() {
-	r1 := Rectangle{12, 2}
-	r2 := Rectangle{9, 4}
-	c1 := Circle{10}
-	c2 := Circle{25}
-	fmt.Println("Area of r1 is: ", r1.area())
-	fmt.Println("Area of r2 is: ", r2.area())
-	fmt.Println("Area of c1 is: ", c1.area())
-	fmt.Println("Area of c2 is: ", c2.area())
+	boxes := BoxList{
+		Box{4, 4, 4, RED},
+		Box{10, 10, 1, YELLOW},
+		Box{1, 1, 20, BLACK},
+		Box{10, 10, 1, BLUE},
+		Box{10, 30, 1, WHITE},
+		Box{20, 20, 20, YELLOW},
+	}
+
+	fmt.Printf("We have %d boxes in our set\n", len(boxes))
+	fmt.Println("The volume of the first one is", boxes[0].Volume(), "cm³")
+	fmt.Println("The color of the last one is", boxes[len(boxes)-1].color.String())
+	fmt.Println("The biggest one is", boxes.BiggestColor().String())
+
+	fmt.Println("Let's paint them all black")
+	boxes.PaintItBlack()
+	fmt.Println("The color of the second one is", boxes[1].color.String())
+
+	fmt.Println("Obviously, now, the biggest one is", boxes.BiggestColor().String())
 }
