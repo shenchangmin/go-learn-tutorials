f01f504dbd853d27fe7d735d2134d28ad66004f1
shenchangmin <503228482@qq.com>
2017-01-13 18:24:40 +0800

    修改interface

--- src/tutorials/interface/main.go
+++ src/tutorials/interface/main.go
@@ -2,78 +2,75 @@ package main
 
 import "fmt"
 
-// Human 基础类
 type Human struct {
 	name  string
 	age   int
 	phone string
 }
 
-// Student 学生类
 type Student struct {
-	Human
+	Human  //匿名字段
 	school string
 	loan   float32
 }
 
-// Employee 雇主类
 type Employee struct {
-	Human
+	Human   //匿名字段
 	company string
 	money   float32
 }
 
-// SayHi Human对象实现SayHi方法
-func (h *Human) SayHi() {
-	fmt.Printf("Hi,I am %s you can call me on %s\n", h.name, h.phone)
+//Human实现SayHi方法
+func (h Human) SayHi() {
+	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
 
-// Sing Human对象实现Sing方法
-func (h *Human) Sing(lyrics string) {
+//Human实现Sing方法
+func (h Human) Sing(lyrics string) {
 	fmt.Println("La la la la...", lyrics)
 }
 
-//Guzzle Human的方法
-func (h *Human) Guzzle(beerStein string) {
-	fmt.Println("Guzzle Guzzle Guzzle...", beerStein)
+//Employee重载Human的SayHi方法
+func (e Employee) SayHi() {
+	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
+		e.company, e.phone)
 }
 
-//SayHi Employee对象重载SayHi方法
-func (e *Employee) SayHi() {
-	fmt.Printf("Hi,I am %s,I work at %s.Call me on %s\n", e.name, e.company, e.phone)
-}
-
-//BorrowMoney Student实现BorrowMoney方法
-func (s *Student) BorrowMoney(amount float32) {
-	s.loan += amount // (again and again and...)
-}
-
-//SpendSalary Employee实现SpendSalary方法
-func (e *Employee) SpendSalary(amount float32) {
-	e.money -= amount // More vodka please!!! Get me through the day!
-}
-
-//Men 定义interface
+// Interface Men被Human,Student和Employee实现
+// 因为这三个类型都实现了这两个方法
 type Men interface {
 	SayHi()
 	Sing(lyrics string)
-	Guzzle(beerStein string)
-}
-
-// YoungChap 年轻人活动
-type YoungChap interface {
-	SayHi()
-	Sing(song string)
-	BorrowMoney(amount float32)
-}
-
-// ElderlyGent 老年人活动
-type ElderlyGent interface {
-	SayHi()
-	Sing(song string)
-	SpendSalary(amount float32)
 }
 
 func main() {
+	mike := Student{Human{"Mike", 25, "222-222-XXX"}, "MIT", 0.00}
+	paul := Student{Human{"Paul", 26, "111-222-XXX"}, "Harvard", 100}
+	sam := Employee{Human{"Sam", 36, "444-222-XXX"}, "Golang Inc.", 1000}
+	tom := Employee{Human{"Tom", 37, "222-444-XXX"}, "Things Ltd.", 5000}
 
+	//定义Men类型的变量i
+	var i Men
+
+	//i能存储Student
+	i = mike
+	fmt.Println("This is Mike, a Student:")
+	i.SayHi()
+	i.Sing("November rain")
+
+	//i也能存储Employee
+	i = tom
+	fmt.Println("This is tom, an Employee:")
+	i.SayHi()
+	i.Sing("Born to be wild")
+
+	//定义了slice Men
+	fmt.Println("Let's use a slice of Men and see what happens")
+	x := make([]Men, 3)
+	//这三个都是不同类型的元素，但是他们实现了interface同一个接口
+	x[0], x[1], x[2] = paul, sam, mike
+
+	for _, value := range x {
+		value.SayHi()
+	}
 }
