73a0794802535245f9b15c5a69d0348cf78ca3a7
shenchangmin <503228482@qq.com>
2017-01-17 19:04:47 +0800

    comma-ok断言

--- src/tutorials/interface/main.go
+++ src/tutorials/interface/main.go
@@ -5,17 +5,37 @@ import (
 	"strconv"
 )
 
-//Human 人基础类
-type Human struct {
-	name  string
-	age   int
-	phone string
+//Element 存值
+type Element interface {
 }
 
-func (h Human) String() string {
-	return "<<" + h.name + "-" + strconv.Itoa(h.age) + "years old-" + h.phone + ">"
+//List Element
+type List []Element
+
+//Person class
+type Person struct {
+	name string
+	age  int
+}
+
+//定义了string方法，实现了fmt.stringer
+func (p Person) String() string {
+	return "(name:" + p.name + "- age:" + strconv.Itoa(p.age) + "years)"
 }
 func main() {
-	Bob := Human{"Bob", 39, "000-7777-xx"}
-	fmt.Println("This Human is : ", Bob)
+	list := make(List, 3)
+	list[0] = 1
+	list[1] = "hello"
+	list[2] = Person{"Dennis", 70}
+	for index, element := range list {
+		if value, ok := element.(int); ok {
+			fmt.Printf("list[%d] is an int and its value is %d\n", index, value)
+		} else if value, ok := element.(string); ok {
+			fmt.Printf("list[%d] is a string and its value is %s\n", index, value)
+		} else if value, ok := element.(Person); ok {
+			fmt.Printf("list[%d] is a Person and its value is %s\n", index, value)
+		} else {
+			fmt.Printf("list[%d] is of a different type\n", index)
+		}
+	}
 }
